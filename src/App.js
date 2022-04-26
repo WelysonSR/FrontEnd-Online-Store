af2d609fb861500badea88ccddf781c70f0c9f4d
Welyson <welysonsr@gmail.com>
2022-04-26 16:31:26 -0300

    2º requsito concluido

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import TelaPrincioal from './components/TelaPrincioal';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ TelaPrincioal } />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
