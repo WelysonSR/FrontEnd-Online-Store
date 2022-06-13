98d2c8c0d7748d8df8ce6dd06d00fac2a5a51bd5
Welyson <welysonsr@gmail.com>
2022-06-13 18:45:32 -0300

    convertendo ListaAvaliação para função

--- src/components/ListaAvaliacao.jsx
+++ src/components/ListaAvaliacao.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class ListaAvaliacao extends React.Component {
-  render() {
-    const { avaliacao } = this.props;
-
-    return (
-      <section>
-        <p>{avaliacao.email}</p>
-        <p>{`Nota: ${avaliacao.nota}`}</p>
-        <p>{avaliacao.descricao}</p>
-        <hr />
-      </section>
-    );
-  }
+function ListaAvaliacao({ avaliacao }) {
+  return (
+    <section>
+      <p>{avaliacao.email}</p>
+      <p>{`Nota: ${avaliacao.nota}`}</p>
+      <p>{avaliacao.descricao}</p>
+      <hr />
+    </section>
+  );
 }
 
 ListaAvaliacao.propTypes = {
