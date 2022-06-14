05e3c254b7fb925e096171feed6af29c3a21d66b
WelysonSR <WelysonSR@users.noreply.github.com>
2022-06-14 02:34:43 +0000

    deploy: f1af3cf708157a4d5ee6a32dcc941d303def50f9

---
+++ precache-manifest.fc66256a2fadf9333fc9d14ad49b5c65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13710fcc42e71314cdd007157e2d9b63",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3809759709c403693bd8",
+    "url": "./static/css/main.ad611353.chunk.css"
+  },
+  {
+    "revision": "11afbbd91d703fa6b8d9",
+    "url": "./static/js/2.15ebace8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.15ebace8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3809759709c403693bd8",
+    "url": "./static/js/main.0080192d.chunk.js"
+  },
+  {
+    "revision": "3bad4da90a1d62b2ed0b",
+    "url": "./static/js/runtime-main.6cc6b37c.js"
+  }
+]);
